4e16c3150992291b10920b60b7d618e5e354143d
Shravya N <109436935+shravyaN25@users.noreply.github.com>
2024-05-31 13:49:03 +0530

    Shra Quadratic_eqn.c

--- Quadratic_eqn.c
+++ Quadratic_eqn.c
@@ -1,4 +1,4 @@
-#include <math.h>
+11#include <math.h>
 #include <stdio.h>
 void displayResult(double discriminant, double a, double b, double root1, double root2, double realPart, double imagPart) {
   if (discriminant > 0) {
